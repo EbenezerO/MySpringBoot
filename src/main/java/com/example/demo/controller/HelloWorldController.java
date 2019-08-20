9360ed7509cdb8a30491bf96cd2cabf25d52ea8b
Ouyang <1305805709@qq.com>
2019-08-20 08:39:03 +0800

    add dom4j ->XML

--- src/main/java/com/example/demo/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.Dom4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +9,11 @@ public class HelloWorldController {
     @RequestMapping("/hello")
     public String index() {
         return "Hello World";
+    }
+
+    @RequestMapping("/getXML")
+    public String get() {
+        Dom4j t=new Dom4j();
+        return t.getXML();
     }
 }
---
+++ src/main/java/com/example/demo/model/Dom4j.java
@@ -0,0 +1,50 @@
+package com.example.demo.model;
+
+import org.apache.tomcat.jni.File;
+import org.dom4j.*;
+import org.dom4j.io.SAXReader;
+
+import java.util.Iterator;
+import java.util.List;
+
+
+public class Dom4j {
+    public String getXML(){
+        String res="";
+        try {
+            SAXReader reader = new SAXReader();
+            Document doc = reader.read("D:\\Idea\\workspace\\demo\\src\\main\\java\\com\\example\\demo\\model\\pm.xml");
+            // 获取根节点
+            Element root = doc.getRootElement();
+            // 获取根节点下的子节点measData
+            Element it1 = root.element("measData");
+            Element it2 = it1.element("measInfo");
+
+            List key = it2.elements("measType");
+
+            Element it3 = it2.element("measValue");
+            List value = it3.elements("r");
+            Iterator<?> itk = key.iterator(), itv = value.iterator();
+            while ( itk.hasNext())
+            {
+                Element ek = (Element) itk.next();
+                Element ev = (Element) itv.next();;
+                res+=(ek.getStringValue()+":"+ev.getStringValue()+"\r\n");
+            }
+
+
+        }catch (DocumentException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return res;
+
+    }
+
+    public static void main(String[] args) {
+        Dom4j t=new Dom4j();
+        System.out.println(t.getXML());
+    }
+
+}
